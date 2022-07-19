5d53d57b00812b5983f2b2c9b8654061584a1abc
rameem2003 <rameem2019@gmail.com>
2022-07-19 21:44:05 +0600

    Upload

---
+++ src/java_jakat_calculator/Java_jakat_calculator.java
@@ -0,0 +1,14 @@
+
+package java_jakat_calculator;
+
+public class Java_jakat_calculator {
+
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        jakat_app app = new jakat_app();
+        
+        app.show();
+    }
+    
+}
